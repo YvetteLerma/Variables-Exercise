7b55e706279f1cef6c26e36090233ba96e0cd7fe
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-14 13:50:48 -0600

    cat stuff

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Variables Exercise");
+
+            string catName = "Henry";
+            int catAge = 1;
+            char catInitial = 'H';
+            bool isFemale = false;
+            double hisWeight = 2.6;
+            decimal hisPawSize = 1.2m;
+            Console.WriteLine($"My cats name is {catName}, He is {catAge} years old. His initial is {catInitial}. He is a girl which is {isFemale}. He weighs about {hisWeight} lbs. His paw size is {hisPawSize} cm.");
+
         }
     }
 }
